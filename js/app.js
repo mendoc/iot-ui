e5a2dca47ff9dc444c7985fc20b8f05223741449
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-06-28 17:30:06 +0000

    Cree le modele-2x2-on-off

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@
             updateScreen(ui);
             saveData(d);
         } catch (e) {
-
         }
     });
 
@@ -20,18 +19,68 @@
 
     function updateScreen(data) {
         console.log(data);
-        
+
+        // Gestion générale de la fenêtre
         el("#cadre").fond(data?.fond);
         el(".titre h1").texte(data?.titre);
+
+        // Gestion des widgets
+        const widgets = data?.widgets || [];
+        let corps = el(".corps");
+        corps.vider();
+        widgets.forEach(w => {
+            let modeleID = w.modele
+            let modele = el(`#${modeleID}`).cloneNode(true);
+            if (!modele) {
+                return
+            }
+            const keys = Object.keys(w);
+            for (let i = 0; i < keys.length; i++) {
+                if (keys[i] === "modele") continue;
+                else if (keys[i] === "icone") {
+                    const i = modele.querySelector("i.icone");
+                    i.classList.className = "";
+                    i.classList.add("fa-solid", "fa-2xl", `fa-${w.icone}`)
+                }
+                else if (keys[i] === "active") {
+                    if (w.active === true)
+                        modele.querySelector("div.wrapper").classList.add("active");
+                    else
+                        modele.querySelector("div.wrapper").classList.remove("active");
+                } else {
+                    const opt = modele.querySelector(`.${keys[i]}`);
+                    opt.textContent = w[keys[i]];
+                }
+            }
+            corps.ajouter(modele.innerHTML);
+        });
     }
 
     function el(seleteur) {
         let element = document.querySelector(seleteur);
+        if (!element) return null;
+
         element.fond = function (couleur) {
             element.style.backgroundColor = couleur;
         }
         element.texte = function (t) {
             element.textContent = t;
+        }
+        element.html = function (h) {
+            if (h) {
+                element.innerHTML = h;
+            } else {
+                return element.innerHTML;
+            }
+        }
+        element.vider = function () {
+            element.innerHTML = "";
+        }
+        element.ajouter = function (e) {
+            element.innerHTML += e;
+        }
+        element.copier = function () {
+            element.innerHTML += e;
         }
         return element;
     }
