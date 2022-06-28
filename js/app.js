0816f67fa08ad031513d78980223c07554726ad3
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-06-28 10:04:18 +0000

    Enregistre les donnees dans le localstorage

--- js/app.js
+++ js/app.js
@@ -4,27 +4,44 @@
     editor.session.setMode("ace/mode/json");
     editor.on("change", function (e) {
         try {
-            const ui = JSON.parse(editor.getValue())
-            updateScreen(ui)
+            const d = editor.getValue()
+            const ui = JSON.parse(d)
+            updateScreen(ui);
+            saveData(d);
         } catch (e) {
 
         }
     });
 
+    // Au chargement de la page on récupère la dernière sauvegarde
+    const firstData = getData();
+    updateScreen(firstData);
+    editor.setValue(firstData, 1);
+
     function updateScreen(data) {
         console.log(data);
-        el("#cadre").fond(data?.fond)
-        el(".titre h1").texte(data?.titre)
+        
+        el("#cadre").fond(data?.fond);
+        el(".titre h1").texte(data?.titre);
     }
 
     function el(seleteur) {
         let element = document.querySelector(seleteur);
         element.fond = function (couleur) {
-            element.style.backgroundColor = couleur
+            element.style.backgroundColor = couleur;
         }
         element.texte = function (t) {
-            element.textContent = t
+            element.textContent = t;
         }
         return element;
+    }
+
+    function saveData(json) {
+        localStorage.setItem('data', json);
+    }
+
+    function getData() {
+        const data = localStorage.getItem('data');
+        return (!data) ? "{}" : data;
     }
 })();
